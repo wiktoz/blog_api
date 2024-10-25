0d4ed74e86c4b35f805c3ff7e3fddbf1f264905e
bartosz.ziemba <bartoszzziemba@gmail.com>
2024-10-25 22:52:42 +0200

    dockerise for developement

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def greet():
-    return jsonify(message="Hello world!")
+    return jsonify(message="Hello worldaaaaaaa")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
