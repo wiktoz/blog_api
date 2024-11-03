a9762cbc84e1a2400967a25ac97abb01f1bae20e
bartosz.ziemba <bartoszzziemba@gmail.com>
2024-11-03 22:11:55 +0100

    new db Models

--- src/main/db/models.py
+++ src/main/db/models.py
@@ -1,18 +1,33 @@
 from src.main.extensions import db, argon2
+from datetime import datetime
 from email_validator import validate_email, EmailNotValidError
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+user_group = db.Table('user_group', Base.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('users.user_id'), primary_key=True),
+    db.Column('group_id', db.Integer, db.ForeignKey('groups.group_id'), primary_key=True)
+)
 
 class User(db.Model):
+    __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     name = db.Column(db.String(32), nullable=False)
     surname = db.Column(db.String(32))
     password_hash = db.Column(db.String, nullable=False)
+    groups = db.relationship('Group', secondary=user_group, back_populates='users')
+    posts = db.relationship('Post', backref='user', lazy=True)
+    comments = db.relationship('Comment', backref='user', lazy=True)
+    ratings = db.relationship('Rating', backref='user', lazy=True)
+    notifications = db.relationship('Notification', backref='user', lazy=True)
 
     def set_password(self, password):
         self.password_hash = argon2.generate_password_hash(password)
     
     def verify_password(self, password):
-        return argon2.check_password_hash(self.password_hash,password)
+        return argon2.check_password_hash(self.password_hash, password)
 
     def validate_email(self):
         try:
@@ -20,3 +35,77 @@ class User(db.Model):
             return True
         except EmailNotValidError:
             return False
+
+class Group(db.Model):
+    __tablename__ = 'groups'
+    group_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+    users = db.relationship('User', secondary=user_group, back_populates='groups')
+    posts = db.relationship('Post', backref='group', lazy=True)
+
+    def __repr__(self):
+        return f"Group('{self.name}')"
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    post_id = db.Column(db.Integer, primary_key=True)
+    group_id = db.Column(db.Integer, db.ForeignKey('groups.group_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+    photos = db.relationship('Photo', backref='post', lazy=True)
+    comments = db.relationship('Comment', backref='post', lazy=True)
+    ratings = db.relationship('Rating', backref='post', lazy=True)
+    notifications = db.relationship('Notification', backref='post', lazy=True)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.created_at}')"
+
+
+class Photo(db.Model):
+    __tablename__ = 'photos'
+    photo_id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'), nullable=False)
+    base64 = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Photo('{self.url}', '{self.created_at}')"
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    comment_id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Comment('{self.content}', '{self.created_at}')"
+
+class Rating(db.Model):
+    __tablename__ = 'ratings'
+    rating_id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Rating('{self.rating}', '{self.created_at}')"
+
+class Notification(db.Model):
+    __tablename__ = 'notifications'
+    notification_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    viewed = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Notification('{self.content}', '{self.created_at}')"
--- src/main/utils/password_validator.py
+++ src/main/utils/password_validator.py
@@ -1,5 +1,5 @@
 def is_valid_length(password: str):
-    return True if (len(password)>8 and len(password) <= 64) else False
+    return True if (len(password) >= 8 and len(password) <= 64) else False
 
 def contains_pii(password: str, data: list):
     for k in data:
