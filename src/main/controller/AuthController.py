6367cf562832da0a28078d8d08625edfe546779c
ad1s0n <zdrian.alewski@gmail.com>
2024-11-27 20:17:20 +0100

    http codes

--- src/main/controller/AuthController.py
+++ src/main/controller/AuthController.py
@@ -21,23 +21,23 @@ def register():
     email = data['email']
     associated_data = [name,surname,email]
     if not email:
-        return jsonify({"message":"Provide email"}), 200
+        return jsonify({"message":"Provide email"}), 401
     if not name:
-        return jsonify({"message":"Provide your name"}), 200
+        return jsonify({"message":"Provide your name"}), 401
     if not surname:
-        return jsonify({"message":"Provide your surname"}), 200
+        return jsonify({"message":"Provide your surname"}), 401
 
     if db.session.query(User).filter_by(email=email).first() != None:
-        return jsonify({"message":"User with provided email already exist"}), 200
+        return jsonify({"message":"User with provided email already exist"}), 401
     
     if not is_valid_length(password):
-        return jsonify({"message":"Insecure password. Password's length must be in range <8,64>"}), 200
+        return jsonify({"message":"Insecure password. Password's length must be in range <8,64>"}), 401
     
     if contains_pii(password, associated_data):
-        return jsonify({"message":"Insecure password. Password can't consist of personal information"}), 200
+        return jsonify({"message":"Insecure password. Password can't consist of personal information"}), 401
     
     if is_on_blacklist(password):
-        return jsonify({"message":"Insecure password. Provided password is compromised"}), 200
+        return jsonify({"message":"Insecure password. Provided password is compromised"}), 401
     
     user = User(
         email=email,
@@ -45,14 +45,14 @@ def register():
         surname=surname
     )
     if not user.validate_email():
-        return jsonify({"message":"Invalid email"}), 200
+        return jsonify({"message":"Invalid email"}), 401
     
     user.set_password(password)
     db.session.add(user)
     try:
         db.session.commit()
     except:
-        return jsonify({"message":"User with provided email already exist"}), 200
+        return jsonify({"message":"User with provided email already exist"}), 401
     return jsonify({"message":"Account created"}), 200
     
 @auth_bp.route("/login", methods=["POST"])
@@ -60,10 +60,10 @@ def login():
     data = request.get_json()
     email, password = data["email"], data["password"]
     if not email:
-        return jsonify({"login":False,"message":"Provide email"}), 200
+        return jsonify({"login":False,"message":"Provide email"}), 401
     user = db.session.query(User).filter_by(email=email).first()
     if user == None:
-        return jsonify({"login":False,"message":"User doesn't exist"}), 200
+        return jsonify({"login":False,"message":"User doesn't exist"}), 401
     if user.verify_password(password):
         token = create_access_token(identity=user.user_id)
         refresh_token = create_refresh_token(identity=email)
@@ -71,7 +71,7 @@ def login():
         set_access_cookies(out,token)
         set_refresh_cookies(out,refresh_token)
         return out, 200
-    return jsonify({"login":False,"message":"Invalid credentials"}), 200
+    return jsonify({"login":False,"message":"Invalid credentials"}), 401
 
 @jwt.token_in_blocklist_loader
 def is_revoked(jwt_header, jwt_payload: dict):
