f536f9399624c9fe989f914f045b6ca21d4d8d11
ad1s0n <zdrian.alewski@gmail.com>
2025-01-11 15:12:59 +0100

    notification read

--- src/main/controller/NotificationController.py
+++ src/main/controller/NotificationController.py
@@ -1,5 +1,6 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 from src.main.db.models import Notification
+from src.main.extensions import db
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 notification_bp = Blueprint('notification_bp', __name__, url_prefix='/api/notifications')
@@ -14,3 +15,19 @@ def get_mine_notifications():
         return jsonify({"message":"No notifications"}), 404
     notifications_list = [notification.to_dict() for notification in notifications]
     return jsonify(notifications_list), 200
+
+# notification read, input: notification_id, boolean POST
+@notification_bp.route('/read', methods=['POST'])
+@jwt_required()
+def read_notification():
+    notification_id = request.json.get('notification_id')
+    viewed = request.json.get('viewed')
+    identity = get_jwt_identity()
+    notification = Notification.query.filter_by(notification_id=notification_id).first()
+    if notification == None:
+        return jsonify({"message":"No such notification"}), 404
+    if notification.user_id != identity:
+        return jsonify({"message":"No permission"}), 403
+    notification.viewed = viewed
+    db.session.commit()
+    return jsonify({"message":"Notification read"}), 200
